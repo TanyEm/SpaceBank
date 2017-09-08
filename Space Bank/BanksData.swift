88dd63686c3ff19c39556199b545a6d95451ea68
Tanya Tomchuk <tanyem@protonmail.com>
2017-09-08 15:42:40 +0300

    first commit

---
+++ Space Bank/BanksData.swift
@@ -0,0 +1,9 @@
+//
+//  BanksData.swift
+//  Space Bank
+//
+//  Created by Tanya Tomchuk on 08/09/2017.
+//  Copyright © 2017 Tanya Tomchuk. All rights reserved.
+//
+
+import Foundation
---
+++ Space Bank/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Space Bank
+//
+//  Created by Tanya Tomchuk on 08/09/2017.
+//  Copyright © 2017 Tanya Tomchuk. All rights reserved.
+//
+
+import Foundation
